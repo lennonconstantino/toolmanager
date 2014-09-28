048518657f9271df12e18f9d51772ca0eee6d55e
lennonconstantino <lennonconstantino@gmail.com>
2014-09-28 19:59:35 -0300

    Testing created function testeLixo5

--- src/com/Main.java
+++ src/com/Main.java
@@ -17,6 +17,9 @@ import com.toolmanager.model.Role;
 import com.toolmanager.model.Status;
 import com.toolmanager.model.Task;
 import com.toolmanager.model.User;
+import com.toolmanager.service.BookService;
+import com.toolmanager.service.NoteService;
+import com.toolmanager.service.TaskService;
 import com.toolmanager.service.UserService;
 
 
@@ -144,28 +147,41 @@ public class Main {
         // THE SERVICE LAYER ENCAPSULATES EVERY BEGIN/COMMIT/ROLLBACK
         // 
         UserService userService = new UserService(simpleEntityManager);
-		
-        User user = new User("Oliveil2", "123456", "LennonConstantino",Calendar.getInstance());
-		user.setFirstname("Lennon");
-		user.setLastname("Constantino");
-		user.setBirthday(Calendar.getInstance());
-		user.setLastLogin(Calendar.getInstance());
+		TaskService taskService = new TaskService(simpleEntityManager);
+		BookService bookService = new BookService(simpleEntityManager);
+		NoteService noteService = new NoteService(simpleEntityManager);
+        
+        //User user = new User("Alinio2", "123456", "AllainConstantino",Calendar.getInstance());
+		//user.setFirstname("Allain");
+		//user.setLastname("Constantino");
+		//user.setBirthday(Calendar.getInstance());
+		//user.setLastLogin(Calendar.getInstance());
 		Set<Task> tasks = new HashSet<Task>();
 		Set<Book> books = new HashSet<Book>();
 		Set<Note> notes = new HashSet<Note>();
-		notes.add(new Note("Goals","I did 2 goals yesterday",Calendar.getInstance(), Calendar.getInstance(), Role.OWNER));		
-		books.add(new Book("study","after I get up",Calendar.getInstance(),notes));
-		tasks.add(new Task("Complete all","todo",Calendar.getInstance(),Calendar.getInstance(),0,true,Status.TODO,Role.OWNER,books));
+		//notes.add(new Note("Goals","I did 2 goals yesterday",Calendar.getInstance(), Calendar.getInstance(), 2L, Role.OWNER));		
+		//books.add(new Book("study","after I get up",Calendar.getInstance(),notes));
+		//tasks.add(new Task("Complete all","todo",Calendar.getInstance(),Calendar.getInstance(),0,true,Status.TODO, Role.OWNER, 2L,books));
 		//user.setBooks(books);	
-		user.setTasks(tasks);
+		//user.setTasks(tasks);
 		
-        userService.insert(user);
-
+        //userService.insert(user);
+		//taskService.insert(new Task("Complete all","todo",Calendar.getInstance(),Calendar.getInstance(),0,true,Status.TODO, Role.OWNER, null,null));
+		//bookService.insert(new Book("study","after I get up",Calendar.getInstance(),null));
+		//noteService.insert(new Note("Goals","I did 2 goals yesterday",Calendar.getInstance(), Calendar.getInstance(), null, Role.OWNER));
+		
+		User user = userService.getUserById(2L);
+		
+		notes.add(new Note("Goals","I did 2 goals yesterday",Calendar.getInstance(), Calendar.getInstance(), user.getId_person(), Role.OWNER));
+		user.setNotes(notes);
+		
+		userService.update(user);
+		
         List<User> users = userService.findAll();
         for(User u : users){
             System.out.println(u.getFirstname());
         }
-        System.out.println(user.toString());
+        //System.out.println(user.toString());
         ///
         // ALWAYS NEED TO BE CALLED!
         //
